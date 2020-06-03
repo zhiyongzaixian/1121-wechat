8de2a22eddb008c9be0004ed1fa878513e76b21c
zhiyongzaixian <13852292775@163.com>
2020-06-03 17:23:57 +0800

    'day02'

--- guiguyunyinyue_demo/pages/index/index.js
+++ guiguyunyinyue_demo/pages/index/index.js
@@ -9,6 +9,7 @@ Page({
   data: {
     bannerList: [],
     recommendList: [], // 推荐数据
+    topList: [], // 排行榜的数据
   },
 
   /**
@@ -27,6 +28,24 @@ Page({
     this.setData({
       recommendList: recommendListData.result
     })
+    
+    // 获取排行榜的数据
+    let idxs = [0, 1, 2, 3]; // 取排行榜数据的参数
+    let index = 0; // 指针
+    let topListArr = [];
+    while(index < idxs.length){
+      let topListResult = await request(`/top/list?idx=${index++}`)
+      let topListObj = {name: topListResult.playlist.name, tracks: topListResult.playlist.tracks.slice(0, 3)}
+      topListArr.push(topListObj)
+      this.setData({
+        topList: topListArr
+      })
+    }
+    // 在此处统一更新的话，需要等到所有的额请求全部到达以后才能显示，容器造成白屏
+    // this.setData({
+    //   topList: topListArr
+    // })
+    
   },
 
   /**
