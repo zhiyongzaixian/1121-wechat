8340cd30ab14d5906ffa7e86c870314a780339f1
zhiyongzaixian <13852292775@163.com>
2020-06-12 10:52:17 +0800

    'day08'

---
+++ uniApp_demo/guigushop/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App'
+
+// 关闭生产模式下的一些提示
+Vue.config.productionTip = false
+
+ 
+// 声明App组件代表的是整个应用   mpType： 1) mp: 小程序 2) type: 类型
+App.mpType = 'app' // app： application
+
+// 生成Vue实例
+const app = new Vue({
+    ...App
+})
+
+// 挂载实例 === 原生小程序中App()
+app.$mount()
+
+
+// new Vue({
+// 	el: '#app',
+// 	render: h => h(App)
+// })
---
+++ uniApp_demo/guigushop/pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<view id="indexContainer">
+		<view class="header">
+			<image src="/static/images/logo.png" mode=""></image>
+			<view class="searchInput">
+				<input type="text" placeholder="搜索商品" placeholder-class="placeholder"/>
+			</view>
+			<button>登录</button>
+		</view>
+	</view>
+
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style lang="stylus">
+	#indexContainer
+		.header
+			display flex
+			padding 10upx 0
+			image 
+				width 140upx
+				height 40upx
+				margin 10upx 20upx
+			.searchInput
+				height 60upx
+				background #eee
+				.placeholder
+					font-size 26upx
+					text-align center
+			button
+				width 144upx
+				height 60upx
+				line-height 60upx
+				text-align center
+				font-size 24upx
+				margin 0 10upx
+				color #b4282d
+</style>
